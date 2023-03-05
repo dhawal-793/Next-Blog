e8b6d326c3f229bda1309edc5ee2cdc18426aa0e
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-05 09:53:26 +0530

    created data folder to store contact info and navigation links

---
+++ data/contactInfo.js
@@ -0,0 +1,47 @@
+import { FaLinkedin, FaGithub, FaTelegramPlane } from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi";
+import { BsPersonLinesFill } from "react-icons/bs";
+
+const contactInfo = [
+    {
+        id: 1,
+        name: "LinkedIn",
+        link: "https://www.linkedin.com/in/dhawal793/",
+        icon: <FaLinkedin size={17} />,
+        style: "hover:text-white hover:bg-[#0A66C2]"
+    },
+    {
+        id: 2,
+        name: "GitHub",
+        link: "https://github.com/dhawal-793",
+        icon: <FaGithub size={17} />,
+        style: "hover:text-[#E6ECF1] hover:bg-black"
+
+    },
+    {
+        id: 3,
+        name: "Mail",
+        link: "mailto:vijaydhawal9001@gmail.com",
+        icon: <HiOutlineMail size={17} />,
+        style: "hover:bg-white hover:text-[#EB4334]"
+
+    },
+    {
+        id: 4,
+        name: "Telegram",
+        link: "https://t.me/DV_793",
+        icon: <FaTelegramPlane size={17} />,
+        style: "hover:text-white hover:bg-[#2FA2D8] "
+
+    },
+    {
+        id: 5,
+        name: "Resume",
+        link: "/resume.pdf",
+        icon: <BsPersonLinesFill size={17} />,
+        style: "hover:bg-black hover:text-[#5EEAC5]",
+        download: true
+    }
+];
+
+export default contactInfo;
