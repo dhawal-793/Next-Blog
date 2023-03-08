43b4e702cc88163ad283783315cb50e7a8d337b4
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-03-08 15:44:17 +0530

    Updated _app.js to use layout components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
+import Layout from '@/components/layout'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <Component {...pageProps} />
+  </Layout>
 }
